ace23d22f9b354023f3a80e897bea7a42cfcd60a
Rafi Shaik <rafishaik8208@gmail.com>
2023-04-05 13:21:10 +0530

    after changes

--- index.js
+++ index.js
@@ -21,20 +21,14 @@ const header = document.createElement("div");
 header.classList.add("header-class");
 mainContainer.appendChild(header);
 
-const logoContainer = document.createElement("div");
-header.appendChild(logoContainer);
-
 const logo = document.createElement("img");
 logo.setAttribute("src", "");
 logo.setAttribute("alt", "school logo");
-logoContainer.appendChild(logo);
-
-const detailsCon = document.createElement("div");
-detailsCon.classList.add("details-con");
-header.appendChild(detailsCon);
+header.appendChild(logo);
 
 const schoolDetails = document.createElement("div");
-detailsCon.appendChild(schoolDetails);
+schoolDetails.classList.add("details-con");
+header.appendChild(schoolDetails);
 
 const schoolName = document.createElement("h1");
 schoolName.textContent = "MAHARAJA INTERNATIONAL SCHOOL";
@@ -51,15 +45,12 @@ schoolEmail.classList.add("para");
 schoolEmail.textContent = "Visit us at: www.maharajainternationalschoolujjain.com";
 schoolDetails.appendChild(schoolEmail);
 
-const footer = document.createElement("div");
-detailsCon.appendChild(footer);
-
 const heading = document.createElement("h2");
 heading.textContent = "REPORT CARD";
 heading.classList.add("sub-heading");
-footer.appendChild(heading);
+schoolDetails.appendChild(heading);
 
 const heading2 = document.createElement("h2");
 heading2.classList.add("sub-heading");
 heading2.textContent = "ACADEMIC SESSION: 2022-2023";
-footer.appendChild(heading2);
+schoolDetails.appendChild(heading2);
